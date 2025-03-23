7f2d1562b23d9203897767beb7bd71c3be2c8253
Raghavendra17N <raghava01995@gamil.com>
2025-03-23 21:42:47 +0530

    updated build file

--- main.py
+++ main.py
@@ -13,3 +13,14 @@ def dashboard():
 @app.get("/api/map")
 def map_view():
     return {"message": "Map data is being displayed!"}
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
